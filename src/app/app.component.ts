8660f4e0d0e3f067fcc4f0f140701e65b8d309b4
Kamila <kamila.cerdeira.kc@gmail.com>
2022-11-19 19:14:54 -0300

    feat: cria service, faz primeira requisicao e adiciona pacotes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { MarvelService } from './data-access/marvel.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'marvel';
+  characters: any;
+
+  constructor(private service: MarvelService) {
+    this.getter();
+  }
+
+  getter() {
+    this.service.getCharacters().subscribe((payload: any) => {
+      this.characters = payload.data.results;
+      console.log('DATA >>', payload);
+      console.log('characters >>', this.characters);
+    });
+  }
 }
---
+++ src/app/data-access/marvel.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MarvelService {
+  constructor(private http: HttpClient) {}
+
+  public getCharacters(): Observable<any> {
+    const publicKey = 'fc258a57ea026632af703f5658fb640f';
+
+    return this.http.get(`http://gateway.marvel.com/v1/public/characters`, {
+      params: {
+        apikey: publicKey,
+        limit: 100,
+      },
+    });
+  }
+}
