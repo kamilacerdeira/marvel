566b1727d6d6c9d571f44a842ebc51fb27c04ed9
Kamila <kamila.cerdeira.kc@gmail.com>
2022-11-23 00:33:00 -0300

    feat: cria rotas pagina inicial e pagina de personagem

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharacterComponent } from './components/character/character.component';
+import { ShellComponent } from './components/shell/shell.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ShellComponent,
+  },
+  {
+    path: 'character/:characterId',
+    component: CharacterComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
